5a70f44130fee9c7d17a69aae08c10af92d0b9ad
iwai-0310 <chazesingh@gmail.com>
2023-09-18 17:34:16 +0530

    fixed the ping issue and added fnc for filter by status and save a sever through ngForm

--- front-end/serverapp/src/app/app.component.ts
+++ front-end/serverapp/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { CustomResponse } from './interface/custom-response';
 import { BehaviorSubject, Observable, catchError, map, of, startWith } from 'rxjs';
 import { DataState } from './enum/data-state.enum';
 import {Status} from './enum/status.enum'
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -18,7 +19,9 @@ export class AppComponent implements OnInit {
   readonly DataState=DataState;
   readonly Status=Status;
   private filterSubject =new BehaviorSubject<string>('');
+  private dataSubject =new BehaviorSubject<CustomResponse>(null);
   filterStatus$=this.filterSubject.asObservable();
+  selectedStatus:Status
 
   constructor(private serverService:ServerService){ }
 
@@ -26,10 +29,71 @@ export class AppComponent implements OnInit {
     this.appState$=this.serverService.servers$
     .pipe(
       map(response =>{
+        this.dataSubject.next(response)
         return {dataState:DataState.LOADED_STATE,appData:response}
       }),
       startWith({dataState:DataState.LOADING_STATE}),
       catchError((error:string)=>{
+        return of({dataState:DataState.ERROR_STATE,error})
+      })
+    );
+  }
+//funciton to ping the server
+  pingServer(ipAddress:string): void {
+    this.filterSubject.next(ipAddress);
+    this.appState$=this.serverService.ping$(ipAddress)
+    .pipe(
+      map(response =>{
+        const currentData=this.dataSubject.value.data;
+        if(currentData && currentData.servers){
+        const index= this.dataSubject.value.data.servers.findIndex(server=>
+            server.id===(response.data.server?.id || null))
+            if(index!==-1){currentData.servers[index]=response.data.server}}
+        // this.dataSubject.value.data.servers[index]=response.data.server
+        this.filterSubject.next('');
+        return {dataState:DataState.LOADED_STATE,appData:this.dataSubject.value}
+      }),
+      startWith({dataState:DataState.LOADED_STATE,appData:this.dataSubject.value}),
+      catchError((error:string)=>{
+        this.filterSubject.next('');
+        return of({dataState:DataState.ERROR_STATE,error})
+      })
+    );
+  }
+//function to filter sever by server  up and server down
+  filterServers(status:Status): void {
+    
+    this.appState$=this.serverService.filter$(status,this.dataSubject.value)
+    .pipe(
+      map(response =>{
+       
+        return {dataState:DataState.LOADED_STATE,appData:response}
+      }),
+      startWith({dataState:DataState.LOADED_STATE,appData:this.dataSubject.value}),
+      catchError((error:string)=>{
+       
+        return of({dataState:DataState.ERROR_STATE,error})
+      })
+    );
+  }
+  //function tto save sever info to the db
+  saveServer(serverForm:NgForm): void {
+    
+    this.appState$=this.serverService.save$(serverForm.value as Server)
+    .pipe(
+      map(response =>{
+        const currentData=this.dataSubject.value.data;
+        if(currentData && currentData.servers){
+        const index= this.dataSubject.value.data.servers.findIndex(server=>
+            server.id===(response.data.server?.id || null))
+            if(index!==-1){currentData.servers[index]=response.data.server}}
+        // this.dataSubject.value.data.servers[index]=response.data.server
+        this.filterSubject.next('');
+        return {dataState:DataState.LOADED_STATE,appData:this.dataSubject.value}
+      }),
+      startWith({dataState:DataState.LOADED_STATE,appData:this.dataSubject.value}),
+      catchError((error:string)=>{
+        this.filterSubject.next('');
         return of({dataState:DataState.ERROR_STATE,error})
       })
     );
--- front-end/serverapp/src/app/app.module.ts
+++ front-end/serverapp/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
